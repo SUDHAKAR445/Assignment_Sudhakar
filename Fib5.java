655ae368ac3415ff49ffa7dd8a34acde8d043b14
Sudhakar445 <SUDHAKAR445>
2023-09-02 17:12:42 +0530

    fourth commit

--- Fib5.java
+++ Fib5.java
@@ -1,22 +1,22 @@
 import java.util.*;
 import java.math.*;
-class Fib4
+class Fib5
 {
 	public static void fibnacci(BigInteger f1, BigInteger f2, int n)
 	{
 		if(n>=0)
 		{
-			System.out.print(f1+" ");
 			BigInteger temp = f1.add(f2);
 			f1 = f2;
 			f2 = temp;
 			n--;
 			fibnacci(f1, f2, n);
+			System.out.print(f2+" ");
 		}
 	}
 	public static void main(String[] args)
 	{
 		int n = Integer.parseInt(args[0]);
-		fibnacci(new BigInteger("0"),new BigInteger("1"),n);
+		fibnacci(new BigInteger("-1"),new BigInteger("1"),n);
 	}
 }
---
+++ Fib6.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.math.*;
+class Fib6
+{ 
+	static BigInteger fib1 = new BigInteger("0");
+	static BigInteger fib2 = new BigInteger("0");
+	public static void fibnacci(BigInteger f1, BigInteger f2, int n)
+	{
+		if(n!=0)
+		{
+			System.out.print(f1+" ");
+			BigInteger temp = f1.add(f2);
+			f1 = f2;
+			f2 = temp;
+			n--;
+			fibnacci(f1, f2, n);
+		}
+		else
+		{
+			fib1 = f1;
+			fib2 = f2;
+		}
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the n value : ");
+		int n = sc.nextInt();
+		fibnacci(new BigInteger("0"),new BigInteger("1"),n);
+		int choice=0;
+		while(choice!=1)
+		{
+			System.out.println("Enter the choice : 0 - continue 1- exit ");
+			choice = sc.nextInt();
+			switch(choice)
+			{
+				case 0:
+				{
+					System.out.println("Enter the n value : ");
+					n = sc.nextInt();
+					fibnacci(fib1,fib2,n);
+					break;
+				}
+				case 1:
+					break;
+				default:
+					System.out.println("Enter the valid choice!");
+			}
+			System.out.println();
+		}
+	}
+}
